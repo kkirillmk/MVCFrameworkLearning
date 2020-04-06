5fcd6cce90a43bf6764411a3c3c8914bb4b63717
KMK <kirillmk_kmk@mail.ru>
2020-04-06 18:37:28 +0700

    Completed the fourth part of the lesson

---
+++ application/views/account/login.php
@@ -0,0 +1,10 @@
+<form action="#">
+    <fieldset>
+        <legend>Вход</legend>
+        <p>Логин</p>
+        <input type="text">
+        <p>Пароль</p>
+        <input type="text">
+        <button>Вход</button>
+    </fieldset>
+</form>
